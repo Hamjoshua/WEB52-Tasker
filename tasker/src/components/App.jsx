f699582d3a055f6ea68dd761505ade3a2e68faf0
Biryukov Mark <72654119+Hamjoshua@users.noreply.github.com>
2025-10-30 17:58:48 +0700

    Создана папка для компонентов

--- tasker/src/components/App.jsx
+++ tasker/src/components/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import reactLogo from '../assets/react.svg'
+import viteLogo from '../../public/vite.svg'
 import './App.css'
 
 function App() {
@@ -8,6 +8,7 @@ function App() {
 
   return (
     <>
+      <h1>Проверка</h1>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />

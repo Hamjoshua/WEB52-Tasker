a8622f67c501960146e772099f6123d36c9ffeea
Biryukov Mark <72654119+Hamjoshua@users.noreply.github.com>
2025-11-03 10:52:08 +0700

    Добавление компонента "нет задач"

---
+++ tasker/src/components/NoTasks.jsx
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react'
+
+export default function NoTasks(){
+    return (
+        <div className="no-tasks">
+            <hr />
+                No tasks
+            <hr />
+        </div>
+    )
+}

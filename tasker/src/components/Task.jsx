25028eefd89eb3386708663563a33b862f6f0e0a
Biryukov Mark <72654119+Hamjoshua@users.noreply.github.com>
2025-11-03 10:51:46 +0700

    Исправление компонента "Задача"
    Я вспомнил, что у меня был файл с уже готовыми стилями. Стал брать оттуда все

--- tasker/src/components/Task.jsx
+++ tasker/src/components/Task.jsx
@@ -1,23 +1,27 @@
 import React, { useState, useEffect } from 'react'
-import '../styles/adapt.css'
-import '../styles/base.css'
-import '../styles/components.css'
-import '../styles/fonts.css'
-import '../styles/icons.css'
-import '../styles/layouts.css'
-import '../styles/reset.css'
 
 
-function Task({ task, index, onDelete, onEdit, onView }) {
-  return (
-    <div className="task-card">
-      <h3>{task.title}</h3>
-      <p>{task.about}</p>
-      <div>
-        <button onClick={() => onView(index)}>Просмотр</button>
-        <button onClick={() => onEdit(index)}>Редактировать</button>
-        <button onClick={() => onDelete(index)}>Удалить</button>
-      </div>
-    </div>
-  )
+export default function Task({ task, index, onShare, onDelete, onEdit, onView }) {
+    const [isShowedTask, setIsShowed] = useState(false)
+
+    const handleCardClicked = () => {
+        setIsShowed(!isShowedTask)
+    }
+    
+    return (
+        <div className="card-with-actions">
+            <div className="card" onClick={handleCardClicked}>
+                <span className="task-title">{task.title}</span>
+                <span className="task-about">{task.about}</span>
+            </div>
+
+            <button className="btn-delete icon-delete" onClick={() => onDelete(index)}></button>
+
+            <div className={isShowedTask ? "actions showed" : "actions hidden" }>
+                <button className="btn-icon icon-share" onClick={() => onShare(index)}></button>
+                <button className="btn-icon icon-info" onClick={() => onView(index)}></button>
+                <button className="btn-icon icon-edit" onClick={() => onEdit(index)}></button>
+            </div>
+        </div>
+    )
 }

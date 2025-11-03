ffa956c2d3f3c8bf2e84fdac4d8f1a9cffd85ea9
Biryukov Mark <72654119+Hamjoshua@users.noreply.github.com>
2025-11-03 10:54:18 +0700

    Изменение рута проекта

--- tasker/src/main.jsx
+++ tasker/src/main.jsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './components/App.jsx'
+import TaskHolder from './components/TaskHolder.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+import './styles/adapt.css'
+import './styles/base.css'
+import './styles/components.css'
+import './styles/fonts.css'
+import './styles/icons.css'
+import './styles/layouts.css'
+import './styles/reset.css'
+
+createRoot(document.body).render(
+  
+    <TaskHolder />
+  
 )
